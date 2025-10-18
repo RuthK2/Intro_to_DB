d953568bab4af9f05b80dcf4ac961499adab5291
Shidochiri <ruthwk2@gmail.com>
2025-10-18 17:40:00 +0300

    Updated changes

--- task_2 .sql
+++ task_2 .sql
@@ -1,5 +1,11 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
+CREATE TABLE IF NOT EXISTS Authors (
+    
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Books (
     
     book_id INT PRIMARY KEY,
@@ -10,12 +16,7 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 
 );
-CREATE TABLE IF NOT EXISTS Authors (
-    
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
 
-);
 CREATE TABLE IF NOT EXISTS Customers (
     
     customer_id INT PRIMARY KEY,
